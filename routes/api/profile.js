109801d888c14089c6043b61fa7926aa003c8759
Stephanie <stephaniiehallberg@gmail.com>
2019-05-19 14:48:43 +0200

    Profile API Routes - Delete Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -201,7 +201,7 @@ router.put(
       from,
       to,
       current,
-      description
+      description,
     } = req.body;
 
     const newExp = {
@@ -211,8 +211,8 @@ router.put(
       from,
       to,
       current,
-      description
-    }
+      description,
+    };
 
     try {
       const profile = await Profile.findOne({ user: req.user.id });
@@ -228,5 +228,29 @@ router.put(
     }
   }
 );
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
