e5d55bdc94fff74ac685aeac9ae3bf7e0d9ce195
Stephanie <stephaniiehallberg@gmail.com>
2019-05-19 14:12:02 +0200

    Profile API Routes -  Profiles & Profile By User ID

--- models/User.js
+++ models/User.js
@@ -8,4 +8,4 @@ const UserSchema = new mongoose.Schema({
     date: { type: Date, default: Date.now}
 });
 
-module.exports = User = mongoose.model('user', UserSchema);
+module.exports = User = mongoose.model('users', UserSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const router = express.Router()
-const auth = require('../../middleware/auth')
-const { check, validationResult } = require('express-validator/check')
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
-const Profile = require('../../models/Profile')
-const User = require('../../models/User')
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -14,17 +14,17 @@ router.get('/me', auth, async (req, res) => {
     const profile = await Profile.findOne({ user: req.user.id }).populate(
       'user',
       ['name', 'avatar']
-    )
+    );
 
     if (!profile) {
-      return res.status(400).json({ msg: 'There is no profile for this user' })
+      return res.status(400).json({ msg: 'There is no profile for this user' });
     }
-    res.json(profile)
+    res.json(profile);
   } catch (err) {
-    console.error(err.message)
-    res.status(500).send('Server error')
+    console.error(err.message);
+    res.status(500).send('Server error');
   }
-})
+});
 
 // @route   POST api/profile
 // @desc    Create or update user profile
@@ -44,10 +44,10 @@ router.post(
     ],
   ],
   async (req, res) => {
-    const errors = validationResult(req)
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })
-    };
+      return res.status(400).json({ errors: errors.array() });
+    }
 
     const {
       company,
@@ -61,9 +61,9 @@ router.post(
       facebook,
       twitter,
       instagram,
-      linkedin
+      linkedin,
     } = req.body;
-    
+
     // Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
@@ -74,7 +74,6 @@ router.post(
     if (bio) profileFields.bio = bio;
     if (githubusername) profileFields.githubusername = githubusername;
 
-      
     // Skills - Spilt into array
     if (typeof req.body.skills !== 'undefined') {
       profileFields.skills = skills.split(',').map(skill => skill.trim());
@@ -94,8 +93,8 @@ router.post(
       if (profile) {
         // Update
         profile = await Profile.findOneAndUpdate(
-          { user: req.user.id }, 
-          { $set: profileFields }, 
+          { user: req.user.id },
+          { $set: profileFields },
           { new: true }
         );
 
@@ -107,12 +106,46 @@ router.post(
 
       await profile.save();
       res.json(profile);
-
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
     }
   }
-)
+);
 
-module.exports = router
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user id
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
