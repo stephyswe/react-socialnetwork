b0e47fb769371988b15898b7b9e31a64eb7a9467
Stephanie <stephaniiehallberg@gmail.com>
2019-05-20 21:18:48 +0200

    Posts & Comments - Adding Posts

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,10 @@
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from '../actions/types';
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+} from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -17,13 +23,20 @@ export default function(state = initialState, action) {
         posts: payload,
         loading: false,
       };
+    
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
+        loading: false
+      }
 
     case DELETE_POST:
       return {
         ...state,
         posts: state.posts.filter(post => post._id !== payload),
-        loading: false
-      }
+        loading: false,
+      };
 
     case POST_ERROR:
       return {
@@ -38,7 +51,7 @@ export default function(state = initialState, action) {
         posts: state.posts.map(post =>
           post._id === payload.id ? { ...post, likes: payload.likes } : post
         ),
-        loading: false
+        loading: false,
       };
 
     default:
