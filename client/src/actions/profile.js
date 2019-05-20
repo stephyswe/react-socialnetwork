0a0466fd7c5419885956ed436dc0f353a355fc17
Hallberg <Stephanie.Hallberg@elanders.com>
2019-05-20 10:33:39 +0200

    Dashboard & Profile Management - Create Profile Action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -19,3 +19,40 @@ export const GetCurrentProfile = () => async dispatch => {
 		});
 	}
 };
+
+// Create or update profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+	try {
+		// Since we sending data we need config
+		const config = {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+
+		const res = await axios.post('/api/profile', formData, config);
+
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data
+		});
+
+		// Display Update if edit is true
+		dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+		// Creating new profile, then redirect to dashboard
+		if (!edit) {
+			history.push('/dashboard');
+		}
+	} catch (err) {
+		const errors = err.response.data.errors;
+
+		if (errors) {
+			errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+		}
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+};
