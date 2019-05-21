2cb288e4096a2744396b17264e2284afec5c588e
Stephanie <stephaniiehallberg@gmail.com>
2019-05-21 23:15:40 +0200

    prepare for deploy

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -14,7 +14,7 @@ const ProfileTop = ({
   return (
     <div className="profile-top bg-primary p-2">
       <img className="round-img my-1" src={avatar} alt="" />
-      <h1 className="large">{name}</h1>
+      <h1 className="large profile-header">{name}</h1>
       <p className="lead">
         {status} {company && <span> at {company} </span>}
       </p>
